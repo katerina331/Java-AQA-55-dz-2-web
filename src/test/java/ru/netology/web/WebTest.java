998a3159d849b18a46f744b2b8a338f325d88ba0
Ekaterina <katerina331-441@ya.ru>
2023-05-06 19:41:34 +0300

    Add test

--- src/test/java/ru/netology/web/WebTest.java
+++ src/test/java/ru/netology/web/WebTest.java
@@ -23,11 +23,13 @@ public class WebTest {
 
     @BeforeEach
     void setUp() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
         driver.get("http://localhost:9999");
     }
 
@@ -46,4 +48,38 @@ public class WebTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", driver.findElement(By.className("paragraph")).getText().trim());
     }
 
+    @Test
+    void shouldTestNamePhone() {
+        driver.findElements(By.className("input__control")).get(0).sendKeys("Васильев Василий");
+        driver.findElements(By.className("input__control")).get(1).sendKeys("+56841288746");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", driver.findElement(By.className("paragraph")).getText().trim());
+    }
+
+    @Test
+    void shouldTestBadName() {
+        driver.findElements(By.className("input__control")).get(0).sendKeys("Vasilev Vasiy");
+        driver.findElements(By.className("input__control")).get(1).sendKeys("+56841288746");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", driver.findElement(By.className("input__sub")).getText().trim());
+    }
+
+    @Test
+    void shouldTestBadPhone() {
+        driver.findElements(By.className("input__control")).get(0).sendKeys("Васильев Василий");
+        driver.findElements(By.className("input__control")).get(1).sendKeys("89428583321");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", driver.findElements(By.className("input__sub")).get(1).getText().trim());
+    }
+
+    @Test
+    void shouldTestCheckbox() {
+        driver.findElements(By.className("input__control")).get(0).sendKeys("Васильев Василий");
+        driver.findElements(By.className("input__control")).get(1).sendKeys("+56841288746");
+        driver.findElement(By.className("button")).click();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", driver.findElement(By.className("input_invalid")).getText());
+    }
 }
